73709d110cd3200e4e8673531fbbe5e4ee9a5d4e
YoshimasaToguchiR <27772805+YoshimasaToguchiR@users.noreply.github.com>
2023-02-23 17:01:10 -0500

    js parte 1

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,8 @@
+// var tiene poco alcance//
+//let es una variable que tiene mayor alcance que var//
+//const no se puede cambiar en un futuo//
+
 var contactoYoshi = 123456789;
 var contacto = 'Hola mundo'
+
+
