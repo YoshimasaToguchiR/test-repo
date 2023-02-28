e541aa3047accc2efb1a93d5bd2ae27753fad5f1
YoshimasaToguchiR <27772805+YoshimasaToguchiR@users.noreply.github.com>
2023-02-28 12:26:45 -0500

    modificacion

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+/* tipos de datos: 
+undefined, null, boolean, string,  symbol, number y object */
+
+var  miPrimerVariable = 'Yoshimasa Developer';
+
+console.log(miPrimerVariable);
+
+miPrimerVariable = 20;
+
+console.log(miPrimerVariable);
